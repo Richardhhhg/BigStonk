37a33a6a2655b93a34acd5ddc4541815295c68ab
ChonkMonk <richardh110604@gmail.com>
2024-09-22 09:22:10 -0400

    Updated Some Things

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fa import fundamental_analysis
+import yfinance as yf
+
+def get_tickers() -> list[yf.Ticker]:
+    """ Gets input for list of tickers
+    """
+    ticker_list = []
+    ticker = None
+    while ticker.lower() != 'no':
+        try:  # assuming input string
+            ticker = input("Enter a ticker or Type 'No' to stop")
+            t = yf.Ticker(ticker)
+            if t not in ticker_list:
+                ticker_list.append(t)  # Check if typing no breaks it
+        except:
+            print("Please enter a valid ticker")
+    return ticker_list
 
 if __name__ == '__main__':
     fa = fundamental_analysis()
-    fa.get_tickers()
+    ticker_list = get_tickers()
+    fa.set_tickers_from_list(ticker_list)
     fa.set_analyst_sentiment()
     fa.set_personal_sentiment()
     fa.set_net_sentiment()
--- training_data.py
+++ training_data.py
@@ -1,3 +1,13 @@
+# Purpose of this file is just to generate a train.csv file for the machine learning model to train on
+# meant to be run once and never again
 import yfinance as yf
 import pandas as pd
 from fa import fundamental_analysis
+
+if __name__ == '__main__':
+    fa = fundamental_analysis()
+    ticker_list = ['INSERT LARGE ASS LIST OF TICKERS HERE']
+    fa.set_tickers_from_list(ticker_list)
+    fa.set_analyst_sentiment()
+    fa.set_personal_sentiment()
+    fa.set_net_sentiment()
