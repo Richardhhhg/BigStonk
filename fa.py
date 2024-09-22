6a51ea049b818797982b1e8f045971bf34bf988b
Richard Hong <81711255+Richardhhhg@users.noreply.github.com>
2024-09-22 09:19:57 -0400

    Update fa.py

--- fa.py
+++ fa.py
@@ -15,8 +15,15 @@ class fundamental_analysis:
     - analyst_sentiment: How analysts feel about the tragectory of the stonk, meant to refelct market sentiment
     - personal_sentiment: How our methods feel about tragectory of the stonk
     - net_sentiment: The weighted sentiment score for each stonk
+    - portfolio_analyst_sent: total analyst sentiment for the portfolio
+    - portfolio_personal_sent: total personal sentiment for the portfolio
+    - portfolio_net_sent: total net sentiment for the portfolio
+
+    Constants:
     - ANALYST_SENTIMENT_WEIGHT: how much analyst sentiment is weighted in net sentiment
     - PERSONAL_SENTIMENT_WEIGHT: how much personal sentiment is weighted in net sentiment
+    - ANALYST_SELF_WEIGHT: weighting for total analyst sentiments depending on period. Each period 
+    increases weight exponentially
     """
     ticker_list: list[yf.Ticker]
     analyst_sentiment: dict[yf.Ticker, float]
@@ -25,12 +32,16 @@ class fundamental_analysis:
 
     ANALYST_SENTIMENT_WEIGHT = 0.4
     PERSONAL_SENTIMENT_WEIGHT = 0.6
+    ANALYST_SELF_WEIGHT = 0.5187901
 
     def __init__(self) -> None:
         self.ticker_list = []
         self.analyst_sentiment = {}
         self.personal_sentiment = {}
         self.net_sentiment = {}
+        self.portfolio_analyst_sent = 0.0
+        self.portfolio_personal_sent = 0.0
+        self.protfolio_net_sent = 0.0
 
     def __str__(self) -> None:
         """String representation of the fundamental analysis for a given portfolio
@@ -44,19 +55,22 @@ class fundamental_analysis:
                           for ticker in self.ticker_list])
         return res
 
-    def get_tickers(self) -> None:
+    def set_tickers_from_list(self, list) -> None:
         """ Updates ticker list
         """
-        ticker = None
-        while ticker.lower() != 'no':
-            try:  # assuming input string
-                ticker = input("Enter a ticker or Type 'No' to stop")
-                t = yf.Ticker(ticker)
-                if t not in self.ticker_list:
-                    self.ticker_list.append(t)  # Check if typing no breaks it
-            except:
-                print("Please enter a valid ticker")
+        self.ticker_list = list
     
+    def _set_analyst_scores(self, row, df) -> float:
+        """Takes in row of dataframe and the actual dataframe, returns weighted analyst
+        sentiment for the given row"""
+        ticker_sent = 0.0
+        ticker_sent += df.loc[row, 'strongBuy'] + df.loc[row, 'buy'] * 0.5 \
+                + df.loc[row, 'hold'] * 0.0 + df.loc[row, 'sell'] * -0.5 \
+                + df.loc[row, 'strongSell'] * -1
+        ticker_sent *= self.ANALYST_SELF_WEIGHT ** (row + 1)
+        return ticker_sent
+
+
     def set_analyst_sentiment(self) -> None:
         """Goes through all tickers, returns a value for analyst sentiment of each stonk,
         updates it to self.analyst_sentiment
@@ -64,15 +78,27 @@ class fundamental_analysis:
         # How it works
         # total_sentiment records sum of sentiments of analysts
         # for loop going through each ticker in self.ticker_list
-            # each strong buy adds + 1 to total_sentiment
-            # each buy adds +0.5 to total sentiment
-            # each hold adds +0.0 to total sentiment
-            # each sell adds -0.5 to total sentiment
-            # each strong sell adds -1 to total sentiment
-        # TODO: Decide how many months this should go back, IMPLEMENTT THIS METHOD
+        #   each strong buy adds + 1 to total_sentiment
+        #   each buy adds +0.5 to total sentiment
+        #   each hold adds +0.0 to total sentiment
+        #   each sell adds -0.5 to total sentiment
+        #   each strong sell adds -1 to total sentiment
+        # Then divides based on total amount of recommendations
+        # TODO: Maybe include bootstrap for samples with small amount of recommendations
         total_sentiment = 0.0
         for ticker in self.ticker_list:
             analyst_scores = ticker.get_recommendations()
+            ticker_sent = 0.0
+            scores_count = 0
+            for i in analyst_scores.index:
+                analyst_scores['count'] = analyst_scores.sum(axis = 1, numeric_only= True)
+                ticker_sent += self._set_analyst_scores(i, analyst_scores)
+                score_count += analyst_scores.loc[i, 'count']
+            ticker_sent /= scores_count
+            self.analyst_sentiment[ticker] = ticker_sent
+            total_sentiment += ticker_sent
+        total_sentiment /= len(self.ticker_list)
+                
 
     def set_personal_sentiment(self) -> None:
         """Goes through some more relevant information on yfinance 
@@ -88,7 +114,14 @@ class fundamental_analysis:
             self.net_sentiment[ticker] = fundamental_analysis.ANALYST_SENTIMENT_WEIGHT \
             * self.analyst_sentiment['ticker'] + fundamental_analysis.PERSONAL_SENTIMENT_WEIGHT \
             * self.personal_sentiment['ticker']
-        
+    
+    def create_dataframe(self) -> pd.DataFrame:
+        """Turns data stored inside to a pandas dataframe for future use
+        """
+        df = pd.DataFrame(columns=['Ticker', 'NetSentScore'])
+        for ticker, score in self.net_sentiment.items():
+            pass
+        return df
 
 if __name__ == '__main__':
     import python_ta
@@ -97,9 +130,3 @@ if __name__ == '__main__':
         'max-line-length': 100,
         'disable': ['possibly-undefined']
     })
-
-
-
-        
-            
-
