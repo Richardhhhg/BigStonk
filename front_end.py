781abc66f4f5791ba51c1fc1414bf52d8723a63d
ChonkMonk <richardh110604@gmail.com>
2024-10-03 15:50:30 -0400

    front end done

--- front_end.py
+++ front_end.py
@@ -5,17 +5,19 @@ import yfinance as yf
 import streamlit as st
 import yfinance as yf
 
-def get_tickers() -> tuple[dict, list]:
+def get_tickers() -> tuple[dict, list, str]:
     # Initialize or get the dictionary from session state to persist data across reruns
     if 'ticker_dict' not in st.session_state:
         st.session_state.ticker_dict = {}
     if 'ticker_list' not in st.session_state:
         st.session_state.ticker_list = []
+    if 'status' not in st.session_state:
+        st.session_state.status = "OFF"
 
     # User input for stock ticker and expected return
     ticker = st.text_input("Enter stock ticker:")
     ticker = ticker.upper()
-    expected_return = st.number_input("Enter expected return (%)")
+    expected_return = st.number_input("Enter expected return")
 
     # Button to save the inputs
     if st.button("Add Stock"):
@@ -26,6 +28,7 @@ def get_tickers() -> tuple[dict, list]:
             st.success(f"Added {ticker} with expected return of {expected_return:.2f}")
         elif ticker:
             st.session_state.ticker_list.append(ticker)
+            st.success(f"Added {ticker}!")
         else:
             st.error("Please input ticker, expected return, and confidence")
 
@@ -36,23 +39,17 @@ def get_tickers() -> tuple[dict, list]:
 
     # ending the inputs
     if st.button("KEEL"):
-        return st.session_state.ticker_list, st.session_state.ticker_dict
+        st.session_state.status = "ON"
 
+    return st.session_state.ticker_list, st.session_state.ticker_dict, st.session_state.status
 
 if __name__ == '__main__':
     fa = fundamental_analysis()
 
     st.title('BIG STONKS GO BONK')
-    ticker_list, ticker_dict = get_tickers()
-    # ticker_list = get_tickers()
+    ticker_list, ticker_dict, status = get_tickers()
 
-    if ticker_list and ticker_dict:
-        # fa.set_tickers_from_list(ticker_list)
-        # fa.set_analyst_sentiment()
-        # fa.set_personal_sentiment()
-        # fa.set_net_sentiment()
-        # st.write(fa)
-        # ticker_list = fa.get_tickers_as_str()
+    if status == "ON":
         mvo = mean_var_optimizer(ticker_list, ticker_dict)
         mvo.set_ticker_prices()
         mvo.optimal_portfolio()
